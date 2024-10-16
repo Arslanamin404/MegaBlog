e82c92842195c3c47dbc0833e5e1f16826bcca7c
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-16 12:24:12 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-export const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/authSlice";
+import authService from "./appwriteServices/auth_service";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <div>
-      <h1>Mega Blog Project with Appwrite open source backend service!</h1>
-    </div>
+
+export const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <>
+      <div className="min-h-screen flex flex-wrap justify-center bg-gray-200">
+        <div className="block">
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </>
+  ) : (
+    <p>Loading...</p>
   );
 };
